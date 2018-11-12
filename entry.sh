fb0f71a0aa975f6b2caae5fd6c2a178c1cd37671
benderstwin <benderstwin@users.noreply.github.com>
2018-11-12 15:45:19 -0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install -r "/appdir/config/requirements.txt"
-touch "/appdir/cron.log"
-crontab "/appdir/config/cron" root
+#touch "/appdir/cron.log"
+#crontab "/appdir/config/cron" root
 chmod a+x "/appdir/collectors/*"
-cron -f
+#cron -f
