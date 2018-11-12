3137bd8cae1bcef11b3ee3800cbfdc3e1d69c19c
benderstwin <bryan@bwirth.com>
2018-11-12 12:42:44 -0700

    BETA

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip3 install -r "/appdir/config/requirements.txt"
 crontab "/appdir/config/cron"
-service start cron
+service cron start
+chmod +x /appdir/collectors/
 tail -f /var/log/faillog > /var/log/tail
