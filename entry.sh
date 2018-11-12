6e630ab79221d787946bdee26655c02ea55c70ee
benderstwin <bryan@bwirth.com>
2018-11-12 14:48:08 -0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip3 install -r "/appdir/config/requirements.txt"
+touch "/appdir/cron.log"
 crontab "/appdir/config/cron" root
-chmod a+x /appdir/collectors/*
+chmod a+x "/appdir/collectors/*"
 cron -f
